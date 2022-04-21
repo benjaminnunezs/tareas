a26b1b9cf1e6bfe38d8bf821844395a03884eaa2
yessenia <benjaminnunez.silva@gmail.com>
2022-04-20 22:39:13 -0400

    unoo22

---
+++ segundo_dia.rb
@@ -0,0 +1,12 @@
+#nombre = gets.chomp
+
+#puts "hola #{nombre}!"
+
+#primero = ARGV[0].to_i
+#segundo = ARGV[1].to_i
+#tercero = ARGV[2].to_i
+#puts primero + segundo + tercero
+
+puts 2+2
+puts "hola"+"mundo"
+puts 2+"mundo"
