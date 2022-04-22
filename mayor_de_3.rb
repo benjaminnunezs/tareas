1d3dac78bf6e8230be4a923ba173d94c7f527d02
yessenia <benjaminnunez.silva@gmail.com>
2022-04-21 22:02:25 -0400

    mayorde3

---
+++ mayor_de_3.rb
@@ -0,0 +1,18 @@
+
+ar1 = ARGV[0].to_i
+ar2 = ARGV[1].to_i
+ar3 = ARGV[2].to_i
+
+if(ar1 == ar2 && ar2 == ar3)
+    puts "son todos iguales: #{ar1}"
+end
+
+if(ar1 >= ar2 && ar2 >=ar3)
+    puts"el valor mayor es:#{ar1}"
+end
+
+if(ar2 >= ar1 && ar2 >=ar3)
+puts"el valor mayor es:#{ar2}"
+elsif
+    puts"el valor mayor es:#{ar3}"
+ end   
